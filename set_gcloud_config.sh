2ea7e5ab0fede9ba3cded495f2c522047a6dd2e0
laits1 <thsehdrl94@gmail.com>
2023-08-08 06:17:54 +0000

    test

--- set_gcloud_config.sh
+++ set_gcloud_config.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 export GCLOUD_PATH="/usr/bin/gcloud" # Update this with your actual path
-export GOOGLE_APPLICATION_CREDENTIALS="/home/thsehdrl94/test/jenkins-sa.json" # Update this with your actual path
 export GCP_PROJECT_ID="test-project2-394700" # Update this with your actual project ID
 
 ${GCLOUD_PATH} auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
